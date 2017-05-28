f332995b2944bf2a77a160e9c90f051b58d071d0
axhm3a <axhm3a@gmail.com>
2017-05-28 17:05:06 +0200

    added a few unit tests

--- build.sbt
+++ build.sbt
@@ -9,9 +9,9 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http" % "10.0.6",
   "com.typesafe.akka" %% "akka-persistence" % "2.4.18",
   "org.json4s" %% "json4s-native" % "3.5.2",
-  //"org.json4s" %% "json4s-ext" % "3.5.2"
   "org.json4s" %% "json4s-jackson" % "3.5.2",
-
   "org.iq80.leveldb" % "leveldb" % "0.7",
-  "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8"
+  "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8",
+
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 )
---
+++ src/test/scala/BankAccount/BankAccountSuite.scala
@@ -0,0 +1,31 @@
+/**
+  * Created by axhm3a on 28.05.17.
+  */
+package BankAccount
+
+import org.scalatest.FlatSpec
+
+// http://www.scalatest.org/user_guide/selecting_a_style
+class BankAccountSuite extends FlatSpec {
+  "A new bankaccount" should "have zero balance" in {
+    assert(
+      BankAccountState(
+        List(BankAccountCreated(
+          1,
+          "foo"
+        ))
+      ).getBankAccountBalance(1) == Balance(0))
+  }
+
+  "A deposit of 500" should "should be credited on the balance" in  {
+    val events = List (
+      BankAccountCreated(1, "foo"),
+      AmountDeposited(1, 500.0)
+    )
+
+    assert(
+      BankAccountState(events).getBankAccountBalance(1) ==
+      Balance(500)
+    )
+  }
+}
